f64244d127d03e030504040d3e475c8f85b9efba
JoseIgnacioTapia <josignaciot@gmail.com>
2021-11-01 17:20:26 -0300

    Create, Update & Delete

--- src/context/NotesContext.js
+++ src/context/NotesContext.js
@@ -11,7 +11,7 @@ const NotesProvider = ({ children }) => {
   const [httpError, setHttpError] = useState(null);
 
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [disSubmit, setDisSubmit] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
 
   let urlGET = 'https://notes-app-d2000-default-rtdb.firebaseio.com/notes.json';
 
@@ -43,7 +43,7 @@ const NotesProvider = ({ children }) => {
       setIsLoading(false);
       setHttpError(error.message);
     });
-  }, [urlGET]);
+  }, [urlGET, didSubmit]);
 
   const getNote = async noteID => {
     try {
@@ -62,9 +62,108 @@ const NotesProvider = ({ children }) => {
     }
   };
 
+  const createNote = async () => {
+    setNote(null);
+    setDidSubmit(false);
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch(
+        'https://notes-app-d2000-default-rtdb.firebaseio.com/notes.json',
+        {
+          method: 'POST',
+          body: JSON.stringify({ ...note, updated: new Date() }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong' + response.data);
+      }
+
+      const data = response.json();
+      setIsSubmitting(false);
+      setDidSubmit(true);
+    } catch (error) {
+      setHttpError(error.message);
+    }
+  };
+
+  const updateNote = async noteID => {
+    setDidSubmit(false);
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch(
+        `https://notes-app-d2000-default-rtdb.firebaseio.com/notes/${noteID}.json`,
+        {
+          method: 'PUT',
+          body: JSON.stringify({ ...note, updated: new Date() }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = response.json();
+      setDidSubmit(true);
+      setIsSubmitting(false);
+    } catch (error) {
+      setHttpError(error.message);
+    }
+
+    setNote(null);
+  };
+
+  const deleteNote = async noteID => {
+    setDidSubmit(false);
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch(
+        `https://notes-app-d2000-default-rtdb.firebaseio.com/notes/${noteID}.json`,
+        {
+          method: 'DELETE',
+          body: JSON.stringify(note),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = response.json();
+      setDidSubmit(true);
+      setIsSubmitting(false);
+    } catch (error) {
+      setHttpError(error);
+    }
+
+    setNote(null);
+  };
+
   return (
     <NotesContext.Provider
-      value={{ notes, note, isLoading, httpError, getNote }}
+      value={{
+        notes,
+        note,
+        isLoading,
+        httpError,
+        getNote,
+        setNote,
+        createNote,
+        updateNote,
+        deleteNote,
+      }}
     >
       {children}
     </NotesContext.Provider>
--- src/pages/Note.js
+++ src/pages/Note.js
@@ -6,49 +6,9 @@ import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg';
 import { NoteContainer, NoteHeader } from './NotesStyles';
 
 const Note = ({ match, history }) => {
-  const { note, setNote } = useContext(NotesContext);
+  const { note, setNote, createNote, updateNote, deleteNote } =
+    useContext(NotesContext);
   let noteID = match.params.id;
-
-  const createNote = async () => {
-    await fetch(
-      `https://notes-app-d2000-default-rtdb.firebaseio.com/notes.json`,
-      {
-        method: 'POST',
-        body: JSON.stringify({ ...note, updated: new Date() }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-  };
-
-  const updateNote = async () => {
-    await fetch(
-      `https://notes-app-d2000-default-rtdb.firebaseio.com/notes/${noteID}.json`,
-      {
-        method: 'PUT',
-        body: JSON.stringify({ ...note, updated: new Date() }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-  };
-
-  const deleteNote = async () => {
-    await fetch(
-      `https://notes-app-d2000-default-rtdb.firebaseio.com/notes/${noteID}.json`,
-      {
-        method: 'DELETE',
-        body: JSON.stringify(note),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-
-    history.push('/');
-  };
 
   const changeValueHandler = e => {
     setNote({ ...note, body: e.target.value });
@@ -56,12 +16,18 @@ const Note = ({ match, history }) => {
 
   const submitHandler = () => {
     if (noteID !== 'new' && !note.body) {
-      deleteNote();
+      deleteNote(noteID);
     } else if (noteID !== 'new') {
-      updateNote();
+      updateNote(noteID);
     } else if (noteID === 'new' && note !== null) {
       createNote();
     }
+
+    history.push('/');
+  };
+
+  const deleteHandler = () => {
+    deleteNote(noteID);
 
     history.push('/');
   };
@@ -76,7 +42,7 @@ const Note = ({ match, history }) => {
         </h3>
 
         {noteID !== 'new' ? (
-          <button onClick={deleteNote}>Delete</button>
+          <button onClick={deleteHandler}>Delete</button>
         ) : (
           <button onClick={submitHandler}>Done</button>
         )}
